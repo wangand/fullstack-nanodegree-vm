90ffb3293e3fcaa30ef7be0a0a9a742ac5c555a3
ANDREW WANG <work@ANDREWs-Air.PK5001Z>
2015-11-26 21:42:30 -0800

    Upgraded README file to markdown

---
+++ vagrant/tournament/README.mk
@@ -0,0 +1,33 @@
+# Project 2 Tournament Results
+
+## Installation
+
+Setting up the tournament database:
+1. Navigate to the tournament file in vagrant
+2. Create the tournament database in psql:
+    ``` 
+    psql tournament
+    ```
+3. In the psql command line import tournament.sql:
+    ```
+    \i tournament.sql
+    ```
+4. Exit psql:
+    ```
+    \q
+    ```
+
+## Usage
+
+Running the tests:
+1. Make sure you are in the tournament file in vagrant
+2. Use python to run tournament_test.py from the command line:
+    ```
+    python tournament_test.py
+    ```
+
+## Limitations
+
+* This tournament result system only handles tournaments
+that have an even number of participants.
+* This tournament result system cannot handle draws
